bf1d247ef6c56a021991da180a95afb8b2310037
todor02 <todort02@abv.bg>
2023-07-21 14:50:01 +0300

    git commit

--- A-Z_code/2.Regression/3.PolynomialRegression/polynomial_regression.py
+++ A-Z_code/2.Regression/3.PolynomialRegression/polynomial_regression.py
@@ -8,9 +8,31 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('DataSets/Position_Salaries.csv')
-x = dataset.iloc[:, :-1].values
+x = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+# Training the Linear Regression model on the whole dataset
+from sklearn.linear_model import LinearRegression
+lin_reg_1 = LinearRegression()
+lin_reg_1.fit(x, y)
+
+# Training the Polynomial Regression model on the whole dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree=2)
+x_poly = poly_reg.fit_transform(x)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(x_poly, y)
+
+# Visualising the Linear Regression results
+
+
+# Visualising the Polynomial Regression results
+
+
+# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+
+
+# Predicting a new result with Linear Regression
+
+
+# Predicting a new result with Polynomial Regression
