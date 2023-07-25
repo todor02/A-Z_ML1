566c82d4f3118accd490b06a56e083947087f9ff
todor02 <todort02@abv.bg>
2023-07-26 00:14:18 +0300

    git commit

--- A-Z_code/2.Regression/5.DecisionTreeRegression/decision_tree_regression.py
+++ A-Z_code/2.Regression/5.DecisionTreeRegression/decision_tree_regression.py
@@ -6,25 +6,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('DataSets/Position_Salaries.csv')
-x = dataset.iloc[:, 1:-1].values
+dataset = pd.read_csv('.csv')
+x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# Training the Decision Tree Regression model on the whole dataset
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state=0)
-regressor.fit(x, y)
-
-# Predicting a new result (6.5)
-print(regressor.predict([[6.5]])) # returns: [150000.] (wrong)
-
-# Visualising the Decision Tree Regression results (higher resolution)
-# Needs a higher dimensional dataset!!!
-x_grid = np.arange(min(x), max(x), 0.1)
-x_grid = x_grid.reshape((len(x_grid), 1))
-plt.scatter(x, y, color='red')
-plt.plot(x_grid, regressor.predict(x_grid), color='blue')
-plt.title('Decision Tree Regression')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
-plt.show()
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
