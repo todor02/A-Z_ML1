0c27cd9b9a83b8f4998155ba78454b53e573fcc7
todor02 <todort02@abv.bg>
2023-07-26 23:04:58 +0300

    git commit

--- A-Z_code/2.Regression/8.RegressionModelSelection/decision_tree_regression.py
+++ A-Z_code/2.Regression/8.RegressionModelSelection/decision_tree_regression.py
@@ -7,22 +7,22 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('DataSets/Data.csv')
-X = dataset.iloc[:, :-1].values
+x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
 # Training the Decision Tree Regression model on the Training set
 from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state = 0)
-regressor.fit(X_train, y_train)
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(x_train, y_train)
 
 # Predicting the Test set results
-y_pred = regressor.predict(X_test)
+y_pred = regressor.predict(x_test)
 np.set_printoptions(precision=2)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
 
 # Evaluating the Model Performance
 from sklearn.metrics import r2_score
--- A-Z_code/2.Regression/8.RegressionModelSelection/polynomial_regression.py
+++ A-Z_code/2.Regression/8.RegressionModelSelection/polynomial_regression.py
@@ -7,25 +7,25 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('DataSets/Data.csv')
-X = dataset.iloc[:, :-1].values
+x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
 # Training the Polynomial Regression model on the Training set
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
-poly_reg = PolynomialFeatures(degree = 4)
-X_poly = poly_reg.fit_transform(X_train)
+poly_reg = PolynomialFeatures(degree=4)
+x_poly = poly_reg.fit_transform(x_train)
 regressor = LinearRegression()
-regressor.fit(X_poly, y_train)
+regressor.fit(x_poly, y_train)
 
 # Predicting the Test set results
-y_pred = regressor.predict(poly_reg.transform(X_test))
+y_pred = regressor.predict(poly_reg.transform(x_test))
 np.set_printoptions(precision=2)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
 
 # Evaluating the Model Performance
 from sklearn.metrics import r2_score
