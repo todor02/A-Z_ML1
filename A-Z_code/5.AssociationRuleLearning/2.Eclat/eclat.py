7407f9aa929a193a1342cdd09a3d454dd8e26dc2
todor02 <todort02@abv.bg>
2023-09-11 18:18:13 +0300

    git commit

---
+++ A-Z_code/5.AssociationRuleLearning/2.Eclat/eclat.py
@@ -0,0 +1,9 @@
+# Eclat
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Data Preprocessing
+dataset = pd.read_csv('DataSets/Market_Basket_Optimisation.csv', header=None)
