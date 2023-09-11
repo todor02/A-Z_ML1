0476e1a23ee6b6a26797c18a35f5b57f3d8215dd
todor02 <todort02@abv.bg>
2023-09-11 18:15:48 +0300

    git commit

--- A-Z_code/5.AssociationRuleLearning/1.Apriori/apriori.py
+++ A-Z_code/5.AssociationRuleLearning/1.Apriori/apriori.py
@@ -1,6 +1,7 @@
 # Apriori
 
 # Run the following command in the terminal to install the apyori package: pip install apyori
+# !pip install apyori
 
 # Importing the libraries
 import numpy as np
@@ -8,22 +9,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Data Preprocessing
-dataset = pd.read_csv('DataSets/Market_Basket_Optimisation.csv', header = None)
-transactions = []
+dataset = pd.read_csv('DataSets/Market_Basket_Optimisation.csv', header=None)
+transactionsList = []
 for i in range(0, 7501):
-  transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+  transactionsList.append([str(dataset.values[i,j]) for j in range(0, 20)])
 
 # Training the Apriori model on the dataset
 from apyori import apriori
-rules = apriori(transactions = transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2, max_length = 2)
+rules = apriori(transactions=transactionsList, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2, max_length=2)
 
-# Visualising the results
-
+# Visualising the results:
 # Displaying the first results coming directly from the output of the apriori function
+results = list(rules)
+print(results)
 
 # Putting the results well organised into a Pandas DataFrame
+def inspect(results):
+  lhs         = [tuple(result[2][0][0])[0] for result in results]
+  rhs         = [tuple(result[2][0][1])[0] for result in results]
+  supports    = [result[1] for result in results]
+  confidences = [result[2][0][2] for result in results]
+  lifts       = [result[2][0][3] for result in results]
+  return list(zip(lhs, rhs, supports, confidences, lifts))
+resultsinDataFrame = pd.DataFrame(inspect(results), columns=['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lift'])
 
 # Displaying the results non-sorted
+print("\nNon-sorted:\n")
+print(resultsinDataFrame)
 
 # Displaying the results sorted by descending lifts
-
+print("\nSorted by descending lifts:\n")
+print(resultsinDataFrame.nlargest(n=10, columns='Lift'))
