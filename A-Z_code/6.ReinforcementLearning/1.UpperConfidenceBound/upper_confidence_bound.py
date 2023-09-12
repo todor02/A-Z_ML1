1eacd5dbc448dc30c80312b4d7e0832e53916ca7
todor02 <todort02@abv.bg>
2023-09-13 00:41:27 +0300

    git commit

--- A-Z_code/6.ReinforcementLearning/1.UpperConfidenceBound/upper_confidence_bound.py
+++ A-Z_code/6.ReinforcementLearning/1.UpperConfidenceBound/upper_confidence_bound.py
@@ -6,10 +6,39 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('.csv')
-x = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+dataset = pd.read_csv('DataSets/Ads_CTR_Optimisation.csv')
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+# Implementing UCB
+import math
+N = 10000   # Rounds/Users
+d = 10  # Ads
+ads_selected = []
+numbers_of_selections = [0] * d
+sums_of_rewards = [0] * d
+total_reward = 0
+
+for n in range(0, N):
+    ad = 0
+    max_upper_bound = 0
+    for i in range(0, d):
+        if (numbers_of_selections[i] != 0):
+            average_reward = sums_of_rewards[i] / numbers_of_selections[i]
+            delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
+            upper_bound = average_reward + delta_i
+        else:
+            upper_bound = 1e400     # (1*10)^400
+        if(upper_bound > max_upper_bound):
+            max_upper_bound = upper_bound
+            ad = i
+    ads_selected.append(ad)
+    numbers_of_selections[ad] += 1
+    reward_collected = dataset.values[n, ad]
+    sums_of_rewards[ad] += reward_collected
+    total_reward += reward_collected
+
+# Visualising the results
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Numbers of times each ad was selected')
+plt.show()
