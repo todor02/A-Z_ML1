f713dcdd0073bd985bc219137b7fbd52e713e5ca
todor02 <todort02@abv.bg>
2024-07-03 14:31:37 +0300

    Setting up the Model Selection & Boosting Part

---
+++ A-Z_code/10.ModelSelectionAndBoosting/1.K_Fold_Cross-Validation/k-fold_cross-validation.py
@@ -0,0 +1,16 @@
+# K-Fold Cross Validation
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
